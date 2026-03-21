1b2e1186bcd946f0bba6dd473c620a17329c384a
SarahLasaracina <sarah@botique.me>
2026-03-21 20:03:47 +0100

    start progetto astro

---
+++ src/content/config.ts
@@ -0,0 +1,14 @@
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    heroImage: z.string().optional(),
+    category: z.string().optional(),
+  }),
+});
+
+export const collections = { blog };
