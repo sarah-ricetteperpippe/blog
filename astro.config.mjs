93468717ad06d7d2f8fa98fd48a363212915eb70
SarahLasaracina <ricetteperpippe@gmail.com>
2026-03-23 11:00:33 +0100

    other

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import pagefind from 'astro-pagefind';
 
 export default defineConfig({
   site: 'https://sarah-ricetteperpippe.github.io',
   base: '/blog',
-  integrations: [mdx()],
+  integrations: [mdx(), pagefind()],
 });
